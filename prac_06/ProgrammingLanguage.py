615028f01d01c07872476c253b26fadcd2014e41
CharlotteCorbett <charlotte.corbett@my.jcu.edu.au>
2021-05-06 08:48:45 +1000

    Create ProgrammingLanguage class and write __init__ method

---
+++ prac_06/ProgrammingLanguage.py
@@ -0,0 +1,13 @@
+"""Programming Languages class"""
+
+
+class ProgrammingLanguage:
+    """Intialise a language instance"""
+
+    def __init__(self, name, reflection, typing, year):
+        self.name = name
+        self.reflection = reflection
+        self.typing = typing
+        self.year = year
+
+
