3d0a3db3dd5c9bd5d2055638b90e93a02975484a
CharlotteCorbett <charlotte.corbett@my.jcu.edu.au>
2021-05-06 08:50:40 +1000

    Write is_dynamic() function

--- prac_06/ProgrammingLanguage.py
+++ prac_06/ProgrammingLanguage.py
@@ -13,4 +13,8 @@ class ProgrammingLanguage:
     def __str__(self):
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.reflection, self.typing, self.year)
 
-
+    def is_dynamic(self):
+        if self.typing == 'Dynamic':
+            return True
+        else:
+            return False
