7e2eb30e8acedec6d8108f0f64b85bf51fe5c7d3
CharlotteCorbett <charlotte.corbett@my.jcu.edu.au>
2021-05-12 23:26:00 +1000

    Update completed lists_warmup

---
+++ Prac 4/lists_warmup.py
@@ -0,0 +1,13 @@
+# 1
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers.remove(3)
+numbers.insert(0, 'ten')
+print(numbers)
+# 2
+numbers.remove(2)
+numbers.insert(6, 1)
+print(numbers)
+# 3
+print(numbers[1:6])
+# 4
+print(numbers[5])
